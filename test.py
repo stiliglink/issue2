981ec74f54245eaaa2ca8f7972c791d218379198
stiliglink <stiliglink@gmail.com>
2025-02-12 16:07:03 +0800

    add

--- test.py
+++ test.py
@@ -1,46 +1,26 @@
 import numpy as np
-import numpy as np
-from scipy.integrate import dblquad
 
-me=0.511
+# 设置积分网格大小
+N_theta = 100  # 调整精度 (可选: 100, 200, ...)
+N_phi = 100
 
+# 创建 theta 和 phi 的网格
+theta_vals = np.linspace(0, np.pi, N_theta)
+phi_vals   = np.linspace(0, 2*np.pi, N_phi)
+theta, phi = np.meshgrid(theta_vals, phi_vals)
 
-def w(theta,phi,s):
-    return np.array([[np.exp(-1j*phi/2)*np.cos((theta+(1/2-s))/2)], [np.exp(1j*phi/2)*np.sin((theta+(1/2-s))/2)]])
-def w_dagger(theta,phi,s):
-    return np.array([np.exp(1j*phi/2)*np.cos((theta+(1/2-s))/2), np.exp(-1j*phi/2)*np.sin((theta+(1/2-s))/2)])
+# 函数定义 (你可以替换 test(θ,φ) 为你的函数)
+def test(theta, phi):
+    return np.exp(-10000 * theta) * np.cos(phi)  # 示例函数
 
+# 计算函数值
+f_vals = test(theta, phi)
 
-def u(epsilon, theta, phi, s):
-    res= np.block([[np.sqrt(epsilon + me)*w(theta,phi,s)],[2*np.sqrt(epsilon - me)*s*w(theta,phi,s)]])
-    return res  
+# 第一步: 先对 theta 方向做梯形积分
+int_theta = np.trapz(f_vals, theta_vals, axis=0)
 
-def u_f(epsilon_f,theta_f,phi_f,s_f):
-    res= np.block([np.sqrt(epsilon_f + me)*w_dagger(theta_f,phi_f,s_f),2*np.sqrt(epsilon_f - me)*s_f*w_dagger(theta_f,phi_f,s_f)])
-    return res
+# 第二步: 再对 phi 方向做梯形积分
+integral = np.trapz(int_theta, phi_vals)
 
-v = np.array([[1], [2], [3]])
-s = np.array([1, 2, 3])
-#print()
+print(f"梯形法二重积分结果: {integral}")
 
-
-# 定义复数函数
-def f(x, y):
-    return np.exp(1j * (x + y))  
-
-# 定义积分区间
-a, b = 0, np.pi  # x 的积分区间
-c, d = 0, np.pi  # y 的积分区间
-
-# 使用 dblquad 进行数值积分
-# 注意：dblquad 要求函数签名为 f(y, x)，因此需要调整参数顺序
-result_real, error_real = dblquad(lambda y, x: f(x, y).real, a, b, lambda x: c, lambda x: d)
-result_imag, error_imag = dblquad(lambda y, x: f(x, y).imag, a, b, lambda x: c, lambda x: d)
-
-# 组合实部和虚部
-result = result_real + 1j * result_imag
-error = error_real + 1j * error_imag
-
-# 输出结果
-print("双重积分结果:", result)
-print("误差估计:", error)
